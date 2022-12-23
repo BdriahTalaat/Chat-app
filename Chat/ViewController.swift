e0bd31ae2de276e2271519c0859c8e2f368c9201
BdriahFakhrji <bdriahtalaat@gmail.com>
2022-12-23 23:23:58 +0300

    Initial Commit

---
+++ Chat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Chat
+//
+//  Created by Bdriah Talaat on 29/05/1444 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
